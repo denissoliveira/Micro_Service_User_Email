e08617b53ba6ac23794d7a00c2e2db488ad3cfa2
Denis.oliveira <denis.oliveira@gmail.com>
2025-01-06 22:09:14 -0300

    style: ajustes

--- user/src/main/java/com/ms/user/controllers/UserController.java
+++ user/src/main/java/com/ms/user/controllers/UserController.java
@@ -4,15 +4,21 @@ import com.ms.user.dtos.UserRecordDto;
 import com.ms.user.models.UserModel;
 import com.ms.user.services.UserService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
+
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
     final UserService userService;
 
@@ -20,7 +26,7 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping("/users")
+    @PostMapping
     public ResponseEntity<UserModel> saveUser(@RequestBody @Valid UserRecordDto userRecordDto) {
         var userModel = new UserModel();
         BeanUtils.copyProperties(userRecordDto, userModel);
